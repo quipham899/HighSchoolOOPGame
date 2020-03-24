944d7611db01e601a1baa0a9b2818889abe4d552
thienqui-communistspy <professionalbusiness505@gmail.com>
2020-03-24 14:57:12 -0400

    Addeds some HTML stuff.

--- FlaskPythonOOP/__init__.py
+++ FlaskPythonOOP/__init__.py
@@ -1,8 +1,6 @@
 import os
 
 from flask import Flask
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -31,5 +29,9 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
